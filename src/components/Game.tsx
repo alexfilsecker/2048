04fb08c4d273a7050e813a003338ca90ba5d60af
Alex <alexfilsecker@gmail.com>
2024-10-08 19:11:32 +1000

    minor things

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState, useRef } from "react";
 import { Number } from "./Number";
-import { Grid } from "./Grix";
+import { Grid } from "./Grid";
 import GameLogic, { Dir } from "../Logic/GameLogic";
 
 type GameObject = {
@@ -20,15 +20,11 @@ export function Game() {
   const startX = useRef<number | null>(null);
   const startY = useRef<number | null>(null);
 
-  useEffect(() => {
-    setGame();
-  }, []);
-
-  function setGame() {
+  const setGame = () => {
     setGameObjects([...gameLogic.game.flat(2).sort((a, b) => a.key - b.key)]);
-  }
+  };
 
-  function move(dir: Dir) {
+  const move = (dir: Dir) => {
     const changed = gameLogic.moveDir(dir);
     if (changed) {
       setGame();
@@ -41,56 +37,60 @@ export function Game() {
         setGame();
       }, 150);
     }
-  }
+  };
+
+  const handleKeyDown = (event: KeyboardEvent) => {
+    const keyToDir: { [key: string]: Dir } = {
+      ArrowDown: Dir.DOWN,
+      ArrowUp: Dir.UP,
+      ArrowLeft: Dir.LEFT,
+      ArrowRight: Dir.RIGHT,
+    };
+    if (Object.keys(keyToDir).includes(event.key)) {
+      let key: string = event.key;
+      move(keyToDir[key]);
+    }
+  };
+
+  const handleTouchStart = (event: TouchEvent) => {
+    const touch = event.touches[0];
+    startX.current = touch.clientX;
+    startY.current = touch.clientY;
+  };
+
+  const handleTouchEnd = (event: TouchEvent) => {
+    const touch = event.changedTouches[0];
+    const deltaX = touch.clientX - startX.current!;
+    const deltaY = touch.clientY - startY.current!;
+
+    let dirSwipe: Dir | null = null;
+    if (deltaX > deltaY) {
+      if (deltaX > 0) {
+        dirSwipe = Dir.RIGHT;
+      } else {
+        dirSwipe = Dir.LEFT;
+      }
+    } else {
+      if (deltaY > 0) {
+        dirSwipe = Dir.DOWN;
+      } else {
+        dirSwipe = Dir.UP;
+      }
+    }
+
+    if (dirSwipe !== null) {
+      move(dirSwipe);
+    }
+    startX.current = null;
+    startY.current = null;
+  };
+
+  useEffect(() => {
+    setGame();
+  }, []);
 
   useEffect(() => {
     const element = swipeRef.current;
-
-    const handleKeyDown = (event: KeyboardEvent) => {
-      const keyToDir: { [key: string]: Dir } = {
-        ArrowDown: Dir.DOWN,
-        ArrowUp: Dir.UP,
-        ArrowLeft: Dir.LEFT,
-        ArrowRight: Dir.RIGHT,
-      };
-      if (Object.keys(keyToDir).includes(event.key)) {
-        let key: string = event.key;
-        move(keyToDir[key]);
-      }
-    };
-
-    const handleTouchStart = (event: TouchEvent) => {
-      const touch = event.touches[0];
-      startX.current = touch.clientX;
-      startY.current = touch.clientY;
-    };
-
-    const handleTouchEnd = (event: TouchEvent) => {
-      const touch = event.changedTouches[0];
-      const deltaX = touch.clientX - startX.current!;
-      const deltaY = touch.clientY - startY.current!;
-
-      let dirSwipe: Dir | null = null;
-      if (deltaX > deltaY) {
-        if (deltaX > 0) {
-          dirSwipe = Dir.RIGHT;
-        } else {
-          dirSwipe = Dir.LEFT;
-        }
-      } else {
-        if (deltaY > 0) {
-          dirSwipe = Dir.DOWN;
-        } else {
-          dirSwipe = Dir.UP;
-        }
-      }
-
-      if (dirSwipe !== null) {
-        move(dirSwipe);
-      }
-      startX.current = null;
-      startY.current = null;
-    };
 
     document.addEventListener("keydown", handleKeyDown);
     if (element) {
@@ -107,21 +107,17 @@ export function Game() {
     };
   });
 
-  const numbers = gameObjects.map((gameObject) => {
-    return (
-      <Number
-        value={gameObject.value}
-        row={gameObject.row}
-        col={gameObject.col}
-        key={gameObject.key}
-      />
-    );
-  });
-
   return (
     <div className="grid" ref={swipeRef}>
       <Grid />
-      {numbers}
+      {gameObjects.map((gameObject) => (
+        <Number
+          value={gameObject.value}
+          row={gameObject.row}
+          col={gameObject.col}
+          key={gameObject.key}
+        />
+      ))}
       {loose && (
         <>
           <div className="blur-effect" />
